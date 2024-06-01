b997f506b53f613646af671d567be10fff8a0b43
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-01 21:44:44 +0530

    commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 const Navbar = () =>{
+    const [menu,setMenu]=useState("shop");//setMenu are declearf first for 1 menu
     return(
     <div className="navbar">
      <div className="nav-logo">
@@ -10,14 +11,16 @@ const Navbar = () =>{
      <p>Shopper</p>
     </div>
     <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        {/* setMenu are used to change border state of other menu */}
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Mens{menu === "mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}>Womens{menu === "womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"?<hr/>:<></>}</li>
          </ul>
          <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+            <div className="nav-cart-count">0</div>
          </div>
     </div>
     )
