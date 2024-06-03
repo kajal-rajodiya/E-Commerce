c7d45960a86e3c04d8281e3c5f7255050ff9f6a1
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-03 09:26:06 +0530

    commit

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import Popular from '../Popular/Popular';
 
 const Shop = () => {
   return (
     <div>
       <Hero></Hero>
+      <Popular></Popular>
     </div>
   );
 }
