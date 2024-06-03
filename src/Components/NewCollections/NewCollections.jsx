7d0e7c278c73c1c53bb0013064a5b379a194d803
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-03 11:20:09 +0530

    commit

---
+++ src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NewCollections.css'
+const NewCollections = () => {
+  return (
+    <div className='newcollections'>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className='collections'>
+        {new_collection.map(item,i)=>{
+          
+        }}
+      </div>
+    </div>
+  );
+}
+
+export default NewCollections;
