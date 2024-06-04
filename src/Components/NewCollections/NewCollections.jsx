db1f359750b9a11f0c539680cc5ee92dcd84eb56
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-04 10:56:55 +0530

    commit

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import './NewCollections.css'
+import './NewCollections.css'; // Removed extra space in the import path
+import new_collection from '../Assets/new_collections';
+import Item from '../Item/Item';
+
 const NewCollections = () => {
   return (
-    <div className='newcollections'>
+    <div className='new-collections'>
       <h1>NEW COLLECTIONS</h1>
       <hr />
       <div className='collections'>
-        {new_collection.map(item,i)=>{
-          
-        }}
+        {new_collection.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   );
--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../Hero/Hero';
 import Popular from '../Popular/Popular';
 import Offers from '../Offers/Offers';
+import NewCollections from '../NewCollections/NewCollections';
 
 const Shop = () => {
   return (
@@ -9,6 +10,7 @@ const Shop = () => {
       <Hero></Hero>
       <Popular></Popular>
       <Offers></Offers>
+      <NewCollections />
     </div>
   );
 }
