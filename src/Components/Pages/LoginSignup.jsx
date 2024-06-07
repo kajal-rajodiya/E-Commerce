32482286050fb43fa801fe721027206d020be13f
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-07 12:36:20 +0530

    commit

--- src/Components/Pages/LoginSignup.jsx
+++ src/Components/Pages/LoginSignup.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
-const LoginSignup = () => {
+import React from 'react';
+import './CSS/LoginSignup.css';
 
-return (
-<div className='loginsignup'>
-<div className="loginsignup-container">
-<h1>Sign Up</h1>
-<div className="loginsignup-fields"> 
-<input type="text" placeholder='Your Name' />
-<input type="email" placeholder='Email Address' />
-</div>
-<input type="password" placeholder='Password' />
-<button>Continue</button>
-<p className="loginsignup-login">Already have an account? <span>Login here</span></p>
-<div className='loginsignup-agree'>
-  <input type='checkbox' name='' id='' />
-  <p>By contining, i agree to the terms of use & privacy policy.</p>
-</div>
-</div>
-</div>
+const LoginSignup = () => {
+  return (
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields"> 
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" placeholder='Your Name' />
+
+          <label htmlFor="email">Email Address</label>
+          <input type="email" id="email" placeholder='Email Address' />
+        
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder='Password' />
+        </div>
+
+        <button type="button">Continue</button>
+       
+
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+
+        <div className='loginsignup-agree'>
+          <input type='checkbox' name= '' id='' />
+          {/* <label htmlFor='terms'></label> */}
+          By continuing, I agree to the terms of use & privacy policy.
+        </div>
+      </div>
+    </div>
   );
 }
 
