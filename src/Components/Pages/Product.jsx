0dc7895463c1fae57f6bb4395569c42899bb6396
kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-06-08 12:56:48 +0530

    commit

--- src/Components/Pages/Product.jsx
+++ src/Components/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Breadcrums/Breadcrum';
+import ProductDisplay from '../ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product}= useContext(ShopContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div>
     <Breadcrum product ={product} />
+    <ProductDisplay product = {product} />
     </div>
   );
 }
